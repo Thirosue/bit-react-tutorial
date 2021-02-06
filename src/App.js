9e5748a3b5afbb104573b67bb68ea0488c8ed9e5
Takeshi Hirosue <takeshi.hirosue@bigtreetc.com>
2021-02-06 15:21:11 +0900

    chore: modify image path

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import BaseTemplate from '@bit/thirosue.react-tutorial.base-template';
-import Card from '@bit/thirosue.react-tutorial.card';
+import BaseTemplate from '@bit/thirosue.react-tutorial.base-template'; //<---- 公開したコンポーネントを利用 ※自身の設定に応じて編集
+import Card from '@bit/thirosue.react-tutorial.card'; //<---- 公開したコンポーネントを利用 ※自身の設定に応じて編集
 
 export const items = [
   {
     id: 1,
     title: 'Phone XL',
-    imagePath: 'https://thirosue.github.io/hosting-image/bit-react-tutorial/1.jpg',
+    imagePath: 'https://thirosue.github.io/hosting-image2/bit-react-tutorial/1.jpg',
     description: 'A 1st phone with one of the best screens'
   },
   {
     id: 2,
     title: 'Phone Mini',
-    imagePath: 'https://thirosue.github.io/hosting-image/bit-react-tutorial/2.jpg',
+    imagePath: 'https://thirosue.github.io/hosting-image2/bit-react-tutorial/2.jpg',
     description: 'A 2nd phone with one of the best cameras'
   },
   {
     id: 3,
     title: 'Phone Standard',
-    imagePath: 'https://thirosue.github.io/hosting-image/bit-react-tutorial/3.jpg',
+    imagePath: 'https://thirosue.github.io/hosting-image2/bit-react-tutorial/3.jpg',
     description: 'A 3rd phone with one of the best cameras'
   }
 ];
