bf56d629fdabc8145de5033a297b95d6ebbfedc3
Takeshi Hirosue <takeshi.hirosue@bigtreetc.com>
2021-02-06 14:47:36 +0900

    chore: updte yarn dependencies

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import DialogContent from '@material-ui/core/DialogContent';
-import TopBar from '@bit/thirosue.react-tutorial.top-bar';
-import Footer from '@bit/thirosue.react-tutorial.footer';
+import TopBar from '@bit/thirosue.react-tutorial.top-bar'; //<---- 公開したコンポーネントを利用 ※自身の設定に応じて編集
+import Footer from '@bit/thirosue.react-tutorial.footer'; //<---- 公開したコンポーネントを利用 ※自身の設定に応じて編集
 import theme from './theme';
 
 function App() {
